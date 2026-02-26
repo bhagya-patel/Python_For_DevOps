8dcc4d9e123f03fd58d957739d8596d2d8d712ff
bhagya-patel <imbhagya2005@gmail.com>
2026-02-26 22:53:20 +0530

    feat: add environment validation script

---
+++ Day1/check_env.py
@@ -0,0 +1,14 @@
+# Get the Environment from the User and print it
+
+for i in range(5):
+    env = input("Please enter the environment you are working in: ")
+
+    print(f"The user inputted environment is: {env}")
+
+    if env == "production":
+        print("Don't Deploy On Friday")
+    elif env == "staging":
+        print("Take backup and test well before deploying")
+    else:
+        print("This is a non production environment, you can deploy without worry")
+
